55609989b34c699737ce61cbdbfdc9e81bb59df4
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-03-07 12:23:07 -0300

    style: add new styles in the first section

--- src/Components/FirstSection/styles.ts
+++ src/Components/FirstSection/styles.ts
@@ -5,19 +5,26 @@ export const FirstContainer = styled.section`
 `;
 
 export const InfosContainer = styled.div`
-  
+  border: 1px solid green;
+  width: 77%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
 `;
 
 export const PrincipalText = styled.h1`
-  
+  font-size: 5rem;
+  color: ${(props) => props.theme.colors.yellow};
 `;
 
 export const SubTitle = styled.p`
-  
+  font-size: 2.2rem;
+  color: ${(props) => props.theme.colors.white2};
 `;
 
 export const Button = styled.button`
-  
+  all: unset;
 `;
 
 export const SubStrong = styled.strong`
